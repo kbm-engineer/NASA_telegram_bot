be3c3a56ff35c1ee447d65b53468f32289e22f5f
Boris Kirillov <kirillov_boris_92@mail.ru>
2023-11-14 23:08:13 +0300

    обновлено в класс

--- bot_nasa.py
+++ bot_nasa.py
@@ -11,77 +11,79 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+class RoverNasaAPI:
+    CURIOSITY = 'curiosity'
+    PERSEVERANCE = 'perseverance'
+    CAMERA = 'camera'
 
-async def start(update: Update, context: CallbackContext):
-    user = update.effective_user
-    await update.message.reply_html(
-        f"Привет, {user.mention_html()}! Я маленький бот для получения картинок с Марса. "
-        "Пожалуйста, отправь мне дату в формате 'гггг-мм-дд'. Если что то епадёт не волнуйся, я только учусь."
-    )
+    ROVERS = {
+        CURIOSITY: {
+            CAMERA: [
+                'FHAZ',
+                'RHAZ',
+                'MAST',
+                'CHEMCAM',
+                'MAHLI',
+                'MARDI',
+                'NAVCAM',
+                ]
+        }, 
+        PERSEVERANCE: {
+            CAMERA: [
+                'FRONT_HAZCAM_LEFT_A',
+                'FRONT_HAZCAM_RIGHT_A',
+                'MCZ_LEFT',
+                'MCZ_RIGHT',
+                'NAVCAM_LEFT',
+                'NAVCAM_RIGHT',
+                'REAR_HAZCAM_LEFT',
+                'REAR_HAZCAM_RIGHT',
+                'SKYCAM',
+                'SUPERCAM_RMI'
+                ]
+        },
+    }
+
+    def __init__(self, api_key):
+        self.api_key = api_key
 
 
-async def get_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    rover_name = [
-        'curiosity',
-        'perseverance',
-    ]
+    @staticmethod
+    async def get_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        chat_id = update.effective_chat.id
+        
+        link = f'https://api.nasa.gov/mars-photos/api/v1/rovers/{RoverNasaAPI.CURIOSITY}/photos'
+        api_key = 'DnZoGGbzG4v2pa2rBT1NT2egAWTeSCakqjxVWvf7'
+        earth_date = update.message.text
 
-    link = f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rover_name[1]}/photos'
-    api_key = 'DnZoGGbzG4v2pa2rBT1NT2egAWTeSCakqjxVWvf7'
-    earth_date = update.message.text
-    # camera = [
-    #     'FHAZ',
-    #     'RHAZ',
-    #     'MAST',
-    #     'CHEMCAM',
-    #     'MAHLI',
-    #     'MARDI',
-    #     'NAVCAM',
-    #     ]
-    
-    camera = [
-        "FRONT_HAZCAM_LEFT_A",
-        "FRONT_HAZCAM_RIGHT_A",
-        "MCZ_LEFT",
-        "MCZ_RIGHT",
-        "NAVCAM_LEFT",
-        "NAVCAM_RIGHT",
-        "REAR_HAZCAM_LEFT",
-        "REAR_HAZCAM_RIGHT",
-        "SKYCAM",
-        "SUPERCAM_RMI"
-        ]
+        await context.bot.send_message(chat_id=chat_id, text='Секундочку...')
+        photo_list = []
+        for cam in RoverNasaAPI.ROVERS.get(RoverNasaAPI.CURIOSITY).get(RoverNasaAPI.CAMERA):
+            params = {
+                'api_key': api_key,
+                'earth_date': earth_date,
+                'camera': cam,
+            }
 
-    await context.bot.send_message(chat_id=chat_id, text='Секундочку...')
-    photo_list = []
-    for cam in camera:
-        params = {
-            'api_key': api_key,
-            'earth_date': earth_date,
-            'camera': cam,
-        }
-
+            try:
+                response = requests.get(link, params=params)
+                data = response.json()
+                data_json = data["photos"][0]["img_src"]
+                image_data = requests.get(data_json).content
+                photo_list.append(InputMediaPhoto(media=image_data))
+            except:
+                break
         try:
-            response = requests.get(link, params=params)
-            data = response.json()
-            data_json = data["photos"][0]["img_src"]
-            image_data = requests.get(data_json).content
-            photo_list.append(InputMediaPhoto(media=image_data))
+            await context.bot.send_message(
+                chat_id=chat_id,
+                text=f'Доступность камер: -----------. Загружаем сники...')
+            await context.bot.send_media_group(chat_id=chat_id, media=photo_list)
         except:
-            break
-    try:
-        await context.bot.send_message(
-            chat_id=chat_id,
-            text=f'Доступность камер: {len(photo_list)}/{len(camera)}. Загружаем сники...')
-        await context.bot.send_media_group(chat_id=chat_id, media=photo_list)
-    except:
-        await context.bot.send_message(chat_id=chat_id, text=f'За {earth_date} нет фотографий')
+            await context.bot.send_message(chat_id=chat_id, text=f'За {earth_date} нет фотографий')
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
-    start_handler = CommandHandler('start', start)
-    get_image_handler = MessageHandler(filters.ALL, get_image)
-    application.add_handler(start_handler)
+    get_image_handler = MessageHandler(filters.ALL, RoverNasaAPI.get_image)
     application.add_handler(get_image_handler)
     application.run_polling()
